82f72beaa14ffd4436732a3fd9dc3c46b47829f9
KwonYongKuk <dprimeedu@naver.com>
2024-10-29 09:53:01 +0900

    이번에는 잘 되겠지?

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import streamlit as st
-import pandas as pd
-from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+col1, col2, col3 = st.columns(3)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
